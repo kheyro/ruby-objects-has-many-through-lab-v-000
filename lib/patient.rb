71fdb556dd5f07a2c85db08735ac3917723b8ccc
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-12 19:40:58 +0000

    automatically backed up by learn

---
+++ lib/patient.rb
@@ -0,0 +1,16 @@
+class Patient
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    appointments = []
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def add_appointment(appointment)
+    @appointment << appointment
+  end
+end
