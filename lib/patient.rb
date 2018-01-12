3c9179465b986d02b81d33cabf4050b1935878a5
Denis Atkesone <denis.atkesone@gmail.com>
2018-01-12 19:42:08 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -3,14 +3,5 @@ class Patient
 
   def initialize(name)
     @name = name
-    appointments = []
-  end
-
-  def appointments
-    @appointments
-  end
-
-  def add_appointment(appointment)
-    @appointments << appointment
   end
 end
